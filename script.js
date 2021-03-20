16ca3202633a13b103def3456752c442e9ca04cd
Shreya Nair <73391159+shreyanair612@users.noreply.github.com>
2021-03-20 09:15:56 -0700

    added transitions and worked on fantasy

---
+++ script.js
@@ -0,0 +1,8 @@
+//Fade In-Out Transitions
+//Note: Used help of internet due to weak knowledge of proper JS.
+
+document.addEventListener('DOMContentLoaded', () => {
+      window.setTimeout(function () {
+        document.body.className = '';
+      }, 230);
+    });
