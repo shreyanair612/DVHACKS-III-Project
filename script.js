a9e40622a3a6c8090c3cc9f946454822efa4921d
Shreya Nair <73391159+shreyanair612@users.noreply.github.com>
2021-03-20 17:10:22 -0700

    trying to debug and figure out js :(

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-//Fade In-Out Transitions
 //Note: Used help of internet due to weak knowledge of proper JS.
 
 document.addEventListener('DOMContentLoaded', () => {
-      window.setTimeout(function () {
-        document.body.className = '';
-      }, 230);
-    });
+  window.setTimeout(function () {
+    document.body.className = '';
+  }, 230);
+});
 
 //Form Submission (Code)
+function finalCodeSubmission() {
+  let one = document.getElementById('Q1F').value;
+  let two = document.getElementById('Q2F').value;
+  let three = document.getElementById('Q3F').value;
+  let four = document.getElementById('Q4F').value;
+  let five = document.getElementById('Q5F').value;
+  let six = document.getElementById('Q6F').value;
+  let seven = document.getElementById('Q7F').value;
+  let eight = document.getElementById('Q8F').value;
+  let nine = document.getElementById('Q9F').value;
+  //const codeString1 = `${one} + ${two} + ${three} + ${four}`;
+  //const codeString2 = `${five} + ${six} + ${seven} + ${eight} + ${nine}`;
+  //const codeString = (codeString1 + codeString2);
+
+  //var result = document.getElementById('results');
+
+  //if (codeString == 250910115) {
+  //window.alert('boom.');
+  //} else {
+  //window.alert('not boom.');
+  //}
+}
+
+console.log(nine);
